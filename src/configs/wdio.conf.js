f15dbbaf67d02e59da8cb3d4daf09a8a0cbf73af
Aikokul <baatirbekovna.6@mail.ru>
2025-12-02 12:13:08 +0600

    added allure report

--- src/configs/wdio.conf.js
+++ src/configs/wdio.conf.js
@@ -1,3 +1,5 @@
+import  allure  from 'allure-commandline'
+
 export const config = {
   //
   // ====================
@@ -139,7 +141,39 @@ export const config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ["spec"],
+  reporters: ["spec",
+    ['allure', {
+      outputDir: 'allure-results',
+      disableWebdriverStepsReporting: true, 
+      disableWebdriverScreenshotsReporting: true, 
+    }],
+  ],
+   onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
+
+    afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+    if (error) {
+        await browser.takeScreenshot();
+    }
+},
 
   // Options to be passed to Mocha.
   // See the full list at http://mochajs.org/
